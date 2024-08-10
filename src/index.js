0d50bba6930ca961b778a854e783b1e6464fcd68
Markandey2002 <92726953+Markandey2002@users.noreply.github.com>
2024-08-10 16:47:40 +0530

    Added unique constraint and started cityRepository

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-
 const {PORT} = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
@@ -13,7 +11,7 @@ const setupAndStartServer = async () => {
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extendesc: true}));
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
         
     });
