aa56c17e04262ca36149d9659629513f9bc9fdb3
Markandey2002 <92726953+Markandey2002@users.noreply.github.com>
2024-10-02 16:11:52 +0530

    updated cityservice

---
+++ src/services/city-service.js
@@ -0,0 +1,44 @@
+const {CityRepository} = require('../repository/index');
+
+
+class CityService {
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
+
+    async createCity({name}) {
+        try {
+            const city = await this.cityRepository.createCity({name: name});
+            return city;
+        } catch (error) {   
+            console.log("something went wrong in the service layer");   
+        }
+    }
+    async deleteCity({cityId}) {
+        try {
+            const city = await this.cityRepository.deleteCity({cityId: cityId});
+            return city;
+        } catch (error) {   
+            console.log("something went wrong in the service layer");   
+        }
+    }
+    async updateCity(cityId, data) {
+        try {   
+            const city = await this.cityRepository.updateCity(cityId, data);
+            return city;
+        } catch (error) {   
+            console.log("something went wrong in the service layer");   
+        }        
+    }
+    async getCity(cityId) {
+        try {
+            const city = await this.cityRepository.getCity(cityId);
+            return city;
+        } catch (error) {   
+            console.log("something went wrong in the service layer");   
+        }
+    }        
+
+}
+
+module.exports = CityService;
